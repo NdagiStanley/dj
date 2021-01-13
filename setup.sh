14d90cd4e453acdd11f16c5af4e326edd92ed130
Ndagi Stanley <ndagis@gmail.com>
2021-01-13 17:05:47 +0300

    Update

--- setup.sh
+++ setup.sh
@@ -5,22 +5,25 @@
 __apt__() {
     print_out "Update apt and install apt packages"
     sudo apt update
-    sudo apt install "$1"
+    sudo apt install python3-pip python3-dev nginx curl
 }
 
 __createvenv__() {
+    print_out "Upgrade pip, Create virtual env"
     sudo -H pip3 install --upgrade pip
     sudo -H pip3 install virtualenv
     virtualenv "venv"
 }
 
 __djangomigrate__() {
+    print_out "Makemigrations, Migrate, Collectstatic"
     python manage.py makemigrations
     python manage.py migrate
     python manage.py collectstatic
 }
 
 __allowport__() {
+    print_out "Add rule to open port"
     sudo ufw allow "$1"
 }
 
@@ -34,12 +37,13 @@ print_out() {
 if [ "$1" != "" ] && type "__$1__" &> /dev/null; then
     eval "__$1__"
 elif [ "$1" == "--all" ]; then
-    __apt__ "python3-pip python3-dev nginx curl"
+    __apt__
     __createvenv__
     # shellcheck disable=SC1091
     # we don't have access to the file yet
     source venv/bin/activate
+    python -m pip install -r requirements.txt
     __djangomigrate__
 else
-    echo "Usage: ./setup.sh (apt) | --all)"
+    echo "Usage: ./setup.sh (apt, createvenv, djangomigrate, allowport 'PORT')"
 fi
